e278293afe0d550f8471538f555584f360e9b61b
veroca88 <veronicadonoso1506@gmail.com>
2020-10-02 14:23:53 -0400

    Return to previous cors set up

--- app.js
+++ app.js
@@ -12,27 +12,13 @@ const app = express();
 // require database configuration
 require("./configs/db.configs");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
-
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
